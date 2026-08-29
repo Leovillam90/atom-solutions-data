9608ac2f69ddf1989ff3563021457de6f8766396
Leovillam90 <Info@atomsolutionsdata.com>
2026-08-29 15:10:19 -0500

    feat: optimizacion de tipografia, colores de tarjetas y contenedor enmarcado

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,39 @@
+'use client';
+
+import React from 'react';
 import Seccion1 from './landing/Seccion1';
 import Seccion2 from './landing/Seccion2';
 import Seccion3 from './landing/Seccion3';
 import Seccion4 from './landing/Seccion4';
 import Seccion5 from './landing/Seccion5';
 import Seccion6 from './landing/Seccion6';
-
-// Componente separador neón reutilizable
-function SeparadorNeon() {
-  return (
-    <div className="relative max-w-6xl mx-auto px-6 my-6">
-      <div className="h-px w-full bg-gradient-to-r from-transparent via-[#0DEDC0]/30 to-transparent" />
-    </div>
-  );
-}
+import ExitIntentModal from '@/app/complementos/ExitIntentModal'; // 👈 Importación del modal
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#070B14]">
-      {/* SECCIÓN 1 */}
-      <Seccion1 variante="perspectiveGrid" />
-
-      {/* SEPARADOR 1 */}
+    <div className="min-h-screen bg-[#070B14] w-full overflow-x-hidden">
       
+      {/* RECEPTOR DE INTENTO DE SALIDA DE TRÁFICO (META ADS) */}
+      <ExitIntentModal />
 
-      {/* SECCIÓN 2 */}
-      <Seccion2 variante="spotlightCyan" />
+      {/* SECCIÓN 1: HERO PRINCIPAL */}
+      <Seccion1 variante="atomDynamicGradient" />
 
-      {/* SEPARADOR 2 */}
-     
+      {/* SECCIÓN 2: MARQUEE PAÍSES LATAM */}
+      <Seccion2 variante="circuitBoard" />
 
-      {/* SECCIÓN 3 */}
+      {/* SECCIÓN 3: DIAGNÓSTICO OPERATIVO */}
       <Seccion3 variante="perspectiveGrid" />
 
-      {/* SEPARADOR 3 */}
-      
+      {/* SECCIÓN 4: MÉTRICAS DE CAPITAL PROTEGIDO */}
+      <Seccion4 variante="vignetteDark" />
 
-      {/* SECCIÓN 4 */}
-      <Seccion4 variante="dualAmbient" />
+      {/* SECCIÓN 5: TABLA DE PRECIOS E INVERSIÓN */}
+      <Seccion5 variante="spotlightCyan" />
 
-      {/* SEPARADOR 4 */}
-      
+      {/* SECCIÓN 6: CIERRE ESTRATÉGICO & WIDGET DE ROI */}
+      <Seccion6 variante="hexGrid" />
 
-      {/* SECCIÓN 5 */}
-      <Seccion5 variante="perspectiveGrid" />
-
-      {/* SEPARADOR 5 */}
-     
-
-      {/* SECCIÓN 6 */}
-      <Seccion6 variante="perspectiveGrid" />
     </div>
   );
 }
